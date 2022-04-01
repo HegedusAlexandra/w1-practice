16cd992d9eba40b59aaf3b9596c256d9d7fb28ba
HegedusAlexandra <hexapastry@gmail.com>
2022-04-01 12:25:06 +0200

    2.ora JS

---
+++ frontend/script.js
@@ -0,0 +1,23 @@
+console.log ("Ez az első javascriptem");
+/*
+// a stringeket (szöveg) idézőjelbe kell rakni
+"Ez itt egy szöveg"
+// a dupla és sima macskaköröm csak egy soros stringet (szöveget) enged meg
+'Ez itt egy másik szöveg'
+// backtickkek közé tett szövegben lehet sortörés
+`ez egy több
+soros szöveg(string)`
+// számokat nem kell idézőjelbe rakni
+4
+
+128
+
+0055
+
+1.13
+*/
+function myFirstFunction(){
+    console.log("Ez az első függvényem belsejében levő log")
+}
+
+myFirstFunction()
